ba0ea452d5dd0a9527ba0f61d6e8a4c1ec36b4e9
Filipe Fasolin <ffasolin@gmail.com>
2017-03-29 13:10:01 +0100

    Step 10 - refactoring

--- directory.rb
+++ directory.rb
@@ -1,25 +1,37 @@
-def interactive_menu
-  students = []
-  loop do
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "9. Exit"
-    selection = gets.chomp
-    case selection
-    when "1"
-      students = input_students
-    when "2"
-        print(students)
-        print_footer(students)
-    when "9"
-      exit
-    else
-      puts "I don't know what you meant. Try again."
-    end
+@students = []
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
+end
+
+def show_students
+  print_students_list
+  print_footer
+end
+
+def process(selection)
+  case selection
+  when "1"
+    input_students
+  when "2"
+    show_students
+  when "9"
+    exit
+  else
+    puts "I don't know what you meant. Try again."
   end
 end
 
-def input_students(students=[])
+def interactive_menu
+  loop do
+    print_menu
+    process(gets.chomp)
+  end
+end
+
+def input_students(students = [])
   puts "Please enter the name of the student"
   puts "To finish, just hit return twice"
   tbc = "TBC"
@@ -33,18 +45,17 @@ def input_students(students=[])
     puts "You misspelled the cohort, please type it again"
     cohort = gets.chop
   end
-  students << {name: name, cohort: cohort.to_sym} if name != tbc || cohort != tbc
-  puts "Now we have #{students.count} students" if students.count != 1
-  puts "Now we have #{students.count} student" if students.count == 1
-  input_students(students) unless name == tbc && cohort == tbc
-  students
+  @students << {name: name, cohort: cohort.to_sym} if name != tbc || cohort != tbc
+  puts "Now we have #{students.count} students" if @students.count != 1
+  puts "Now we have #{students.count} student" if @students.count == 1
+  input_students(@students) unless name == tbc && cohort == tbc
 end
 
-def print(students)
-  if students.count > 0
+def print_students_list
+  if @students.count > 0
     puts "The students of Villains Academy"
     puts "--------------"
-    hash_co = students.group_by { |entry| entry[:cohort] }
+    hash_co = @students.group_by { |entry| entry[:cohort] }
     hash_co.each do |key, value|
       puts "#{key.capitalize} cohort"
       for i in 0..value.length-1 do
@@ -54,13 +65,13 @@ def print(students)
   end
 end
 
-def print_footer(names)
-  if names.count > 0
-    puts "Overall, we have #{names.count} great students" if names.count != 1
-    puts "Overall, we have #{names.count} great student" if names.count == 1
+def print_footer
+  if @students.count > 0
+    puts "Overall, we have #{@students.count} great students" if @students.count != 1
+    puts "Overall, we have #{@students.count} great student" if @students.count == 1
   end
 end
 
 interactive_menu
-print(students)
-print_footer(students)
+print(@students)
+print_footer(@students)
